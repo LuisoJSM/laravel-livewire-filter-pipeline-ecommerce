0b35994b254995a9c7cf33cc91c099ed23975ed4
LuisoJSM <luisjosesorianomoreno@gmail.com>
2025-09-19 11:36:06 +0200

    GetProducts working

--- app/Livewire/Shop/Lists/ProductsList.php
+++ app/Livewire/Shop/Lists/ProductsList.php
@@ -5,12 +5,15 @@ namespace App\Livewire\Shop\Lists;
 use App\Models\Product;
 use Livewire\Component;
 use Livewire\WithPagination;
+use Illuminate\Pipeline\Pipeline;
+use App\Enums\Filters\ShopFilters;
 
 class ProductsList extends Component
 {
-
     use WithPagination;
+
     protected string $paginationTheme = 'bootstrap';
+
     private array $filters = [
         'category' => [],
     ];
@@ -19,7 +22,6 @@ class ProductsList extends Component
     {
         $this->resetFilters();
     }
-
 
     private function resetFilters()
     {
@@ -36,36 +38,40 @@ class ProductsList extends Component
          * and pagination is properly reset after updates or reloads.
          */
         collect($this->filters)
-            ->each(fn($filter, $key) => session()->forget('shop' . $key));
+            ->each(fn ($filter, $key) => session()->forget('shop' . $key));
 
         session()->forget('shop:perPage');
 
         $this->gotoPage(1);
     }
 
-
-    private function filters()
+    private function filters(): array
     {
         return collect($this->filters)
-            ->map(fn($filter, $key) => session(key: 'shop' . $key, default: $filter))
+            ->map(fn ($filter, $key) => session('shop' . $key, $filter))
             ->toArray();
     }
 
-
-
     private function getProducts()
     {
-        return Product::query()
-        ->with([
-            'brand:id,name',
-            'category:id,name',
-            'color:id,name',
-            'size:id,name',
-            'reviews:id,product_id,rating',
-        ])
-        ->paginate(session(key:'shop:perPage', default:4));
-
-
+        return app(Pipeline::class)
+            ->send(
+                Product::query()->with([
+                    'brand:id,name',
+                    'category:id,name',
+                    'color:id,name',
+                    'size:id,name',
+                    'reviews:id,product_id,rating',
+                ])
+            )
+            ->through(
+                collect($this->filters())
+                    ->map(fn ($filter, $key) => ShopFilters::from($key)->create($filter))
+                    ->values()
+                    ->all()
+            )
+            ->thenReturn()
+            ->paginate(session('shop:perPage', 4));
     }
 
     public function render()
